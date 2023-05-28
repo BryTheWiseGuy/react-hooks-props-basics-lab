417dca15a7154e1a94b65369a6084b864e050ffb
Brian Ashman <brian.ashman@comcast.net>
2023-05-27 20:04:52 -0700

    Fixes conditional rendering in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio ? props.bio : null}</p>
+      { "" ? null : <p>{props.bio}</p> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
